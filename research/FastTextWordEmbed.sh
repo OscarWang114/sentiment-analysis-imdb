710d3aa0353f6003a8d59fe1986c3193f7726cf2
OscarWang114 <chihhaowang.oscar@gmail.com>
2019-01-01 20:22:13 +0900

    Rename corpus.txt

--- research/FastTextWordEmbed.sh
+++ research/FastTextWordEmbed.sh
@@ -8,6 +8,6 @@ num_workers=4  # Number of threads to run in parallel
 context=10  # Context window size
 downsampling=$(echo $(bc <<< 'scale=3; 1/1000') ) # Downsample setting for frequent words
 
-../../fastText/fasttext skipgram -input corpus.txt \
+../../fastText/fasttext skipgram -input fasttext_corpus.txt \
     -output "${num_features}features_${min_word_count}minwords_${context}context" \
     -dim ${num_features} -minCount ${min_word_count} -thread ${num_workers} -ws ${context} -t ${downsampling}
